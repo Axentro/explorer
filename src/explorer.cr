df9c78aca0e42ee019325e94781911ca1ba75c03
Christian Kakesa <christian.kakesa@gmail.com>
2020-10-01 20:04:02 +0200

    Update the default per page item list

--- src/explorer.cr
+++ src/explorer.cr
@@ -20,7 +20,7 @@ struct Config
     @server : String = "0.0.0.0",
     @port : Int32 = 3100,
     @db : String = "rethinkdb://localhost:28015/explorer_testnet",
-    @per_page = 20
+    @per_page = 15
   )
   end
 end
